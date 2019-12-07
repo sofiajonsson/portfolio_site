4aae3490d09e444dd9c7eb9b198456560b40dc03
Sofia <jonssonsofia93@gmail.com>
2019-12-07 12:31:13 -0800

    cleaned up code

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -33,8 +33,6 @@ class Projects extends Component {
 					</a>
 				</div>
 
-
-
 				<div class="ui padded segment">
 					<div class="content">
 						<p className="general-p-tag">
@@ -58,10 +56,6 @@ class Projects extends Component {
 					</a>
 				</div>
 
-
-
-
-
 				<div class="ui padded segment">
 					<div class="content">
 						<p className="general-p-tag"><a href="https://github.com/sofiajonsson/roam" className="projects-header">Roam Travel App</a> </p>
@@ -69,12 +63,11 @@ class Projects extends Component {
 				<br />
 					<div class="content">
 						<p className="projects-paragraph">
-						Full Ruby on Rails travel social media site <br />
-					 	Used Bcrypt authentication to provide users with secure login feature <br />
-					 	Incorporated nested file features to keep files specific <br />
-					 	Established custom routes for landing page <br />
-						Created a follow to follower relationship for users to “keep up” with other users depending on relationship <br />
-
+							Full Ruby on Rails travel social media site <br />
+						 	Used Bcrypt authentication to provide users with secure login feature <br />
+						 	Incorporated nested file features to keep files specific <br />
+						 	Established custom routes for landing page <br />
+							Created a follow to follower relationship for users to “keep up” with other users depending on relationship <br />
 						</p>
 				 </div>
 				<div class="image" >
@@ -86,10 +79,6 @@ class Projects extends Component {
 					</a>
 						<p> * Note that "Live Site" option  is hosted on a free platform and takes a while to load *</p>
 				</div>
-
-
-
-
 
 				<div class="ui padded segment">
 					<div class="content">
@@ -103,6 +92,7 @@ class Projects extends Component {
 							Utilizes a jwt token for password protection <br />
 						</p>
 				 </div>
+
 				<div class="image" >
 					<img src="https://github.com/sofiajonsson/back-end-calendar-app/raw/master/lib/art/calendar_pic.png" id="project-pic"/>
 				</div>
@@ -110,16 +100,13 @@ class Projects extends Component {
 					View GitHub
 				</a>
 				</div>
+
 				<div>
 				<p className="general-p-tag"> </p>
 					<Link to="/about" className="med-nav-a" id="home-pg-a">
 						Read About Me!
 					</Link>
 				</div>
-
-
-
-
 		</div>
 		)
 	}
