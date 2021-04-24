9f6922e21ff9e9b5152f9aadae027b36f1d596f7
Sofia <jonssonsofia93@gmail.com>
2021-04-24 11:28:52 -0600

    updated about me page

--- src/containers/About.js
+++ src/containers/About.js
@@ -16,14 +16,15 @@ class About extends Component {
 			 <p className="general-p-tag">
 					I'm a Full stack web developer with a passion for travel and languages.
 					<br/> <br/>
-					 With experience in Ruby on Rails, JavaScript, React, Java and a background in business and hospitality,
+					 With experience in Ruby/Rails, JavaScript, React, Java and a background in business and hospitality,
 					  I discovered my passion for code while searching for something to keep my mind stimulated after University.
-						 As a hospitality graduate, I have developed a strong skill set pertaining to organization, communication,
-						 and thriving under pressure. I enjoy learning new things in a fast paced environment and wish to move forward
-						 on this continuous learning path. I am currently employed as an SDET I, and enjoy the challenge of picking up
-						 Java on the job and combing the skills of a QA and Developer to strategize and plan the automation of newly developed functionalities.
+						  I am currently employed as an SDET I, and enjoy the challenge of learning Java on the job where I combine the skills of a QA
+							and Developer to strategize and plan the automation of newly developed functionalities.
 						 Working as an SDET has allowed me to pick up new skills, alter my way of thinking, and really cherish the time I get to spend
-						 coding.
+						 coding. As I progress in my career, I look forward to transitioning into a full time developer role and futhering my knowledge within the realm
+						 of building, rather than testing code. My background as a business and hospitality graduate, have allowed me to develop a strong skill set pertaining to organization, communication,
+						 and thriving under pressure. I enjoy learning new things in a fast paced environment and wish to move forward
+						 on this continuous learning path.
 
 					<br/> <br/>
 					When I can get away from the screen, I am guaranteed to be outside doing activities. Depending on what time of the year it is, that will include skiing, hiking, surfing, etc. and I love bringing my dog Milo with me!
@@ -43,8 +44,7 @@ class About extends Component {
 							</p>
 						<h3 className="about-header-tag">Back-End:</h3>
 							<p className="about-p-tag">
-								Ruby <br/>
-								Rails <br/>
+								Ruby/Rails <br/>
 								Java <br/>
 							</p>
 				    </div>
@@ -60,7 +60,7 @@ class About extends Component {
 				   <div class="column">
 						<h3 className="about-header-tag">Databases:</h3>
 							<p className="about-p-tag">
-								SQL (PostgreSQL)<br/>
+								SQL (PostgreSQL, MySQL)<br/>
 							</p>
 						<h3 className="about-header-tag">Hosting:</h3>
 							<p className="about-p-tag">
