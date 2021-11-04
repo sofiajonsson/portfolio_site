e3beeb354c7ea3061c0dec946c75dca0d93c6769
Sofia <jonssonsofia93@gmail.com>
2021-11-03 22:59:14 -0600

    updated about me snippet

--- src/containers/About.js
+++ src/containers/About.js
@@ -16,16 +16,10 @@ class About extends Component {
 			 <p className="general-p-tag">
 					I'm a Full stack web developer with a passion for travel and languages.
 					<br/> <br/>
-					 With experience in Ruby/Rails, JavaScript, React, Java and a background in business and hospitality,
-					  I discovered my passion for code while searching for something to keep my mind stimulated after University.
-						  I am currently employed as an SDET I, and enjoy the challenge of learning Java on the job where I combine the skills of a QA
-							and Developer to strategize and plan the automation of newly developed functionalities.
-						 Working as an SDET has allowed me to pick up new skills, alter my way of thinking, and really cherish the time I get to spend
-						 coding. As I progress in my career, I look forward to transitioning into a full time developer role and futhering my knowledge within the realm
-						 of building, rather than testing code. My background as a business and hospitality graduate, have allowed me to develop a strong skill set pertaining to organization, communication,
-						 and thriving under pressure. I enjoy learning new things in a fast paced environment and wish to move forward
-						 on this continuous learning path.
-
+					With experience in Ruby/Rails, JavaScript, React, Java, and a background in business and hospitality,
+					I discovered my passion for coding while seeking something to keep my mind stimulated after University.
+					I am currently employed as an SDET and enjoy the challenge of learning and expanding my Java knowledge where I combine the skills of a QA and Developer to strategize and plan the automation of newly developed functionalities.
+					Working as an SDET has allowed me to pick up new skills, alter my way of thinking, and value the time I get to spend coding. As I progress in my career, I look forward to taking on more complex solutions, leveraging modern test automation, and expanding my knowledge of open source frameworks. My background in business and hospitality graduate has allowed me to develop a skill set that centers on organization, communication, and thriving under pressure. I enjoy learning new things in a fast-paced environment and wish to move forward on this continuous learning path.
 					<br/> <br/>
 					When I can get away from the screen, I am guaranteed to be outside doing activities. Depending on what time of the year it is, that will include skiing, hiking, surfing, etc. and I love bringing my dog Milo with me!
 			</p>
