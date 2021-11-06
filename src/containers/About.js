f2b8318718726a6554e5c07ad181c21f9429d2f6
Sofia <jonssonsofia93@gmail.com>
2021-11-06 15:20:22 -0600

    Fixed About.js so that npm run build can run

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { Link } from '
+import { Link } from 'react-router-dom'
+
+class About extends Component {
+
+	render() {
+		
 		return(
 			<div>
 
